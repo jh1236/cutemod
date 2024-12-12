b4ebde0a4ecc047660ceabcaa209192c6cde2820
Jared Healy <healy_jared@yahoo.com>
2024-12-12 22:40:52 +0800

    true last serious commit

--- src/main/java/com/example/mixin/MessageCommandMixin.java
+++ src/main/java/com/example/mixin/MessageCommandMixin.java
@@ -1,19 +1,13 @@
 package com.example.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.MessageCommand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(MessageCommand.class)
-public class ExampleMixin {
+public class MessageCommandMixin {
     @Inject(method = "register", at = @At(value = "HEAD"), cancellable = true)
     private static void injected(CallbackInfo ci) {
         ci.cancel();
