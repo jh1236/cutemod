d1e5a9273996c4c0a3e413a717d487b1d2d765e6
Jared Healy <healy_jared@yahoo.com>
2025-06-25 01:31:41 +0800

    fix da code

--- src/main/java/com/example/mixin/PlayerEntityMixin.java
+++ src/main/java/com/example/mixin/PlayerEntityMixin.java
@@ -3,10 +3,9 @@ package com.example.mixin;
 import com.example.ExampleMod;
 import com.example.IAddNoChecks;
 import net.minecraft.block.Blocks;
+import net.minecraft.component.*;
 import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.block.entity.ChestBlockEntity;
-import net.minecraft.component.ComponentType;
-import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.BundleContentsComponent;
 import net.minecraft.component.type.NbtComponent;
 import net.minecraft.entity.decoration.ItemFrameEntity;
@@ -56,14 +55,21 @@ public abstract class PlayerEntityMixin {
         is.set(DataComponentTypes.CUSTOM_NAME, getDisplayName().copy().append(Text.literal("'s belongings")).setStyle(Style.EMPTY.withColor(Formatting.BLUE)));
         var startPos = ((PlayerEntity) (Object) this).getBlockPos();
         var pos = startPos;
+        if (!world.isInBuildLimit(pos) && pos.getY() > 0) {
+            //we died somewhere where you can't place a chest; so the player can get their own items
+            return;
+        }
         while (!world.getBlockState(pos).isReplaceable()) {
             pos = pos.add(0, 1, 0);
-            if (!world.isInBuildLimit(pos)) { pos = new BlockPos(pos.getX()+1, startPos.getY(), pos.getZ()); }
+            if (!world.isInBuildLimit(pos)) {
+                pos = new BlockPos(pos.getX() + 1, startPos.getY(), pos.getZ());
+            }
         }
 
         world.setBlockState(pos, Blocks.CHEST.getDefaultState());
         var grave = (ChestBlockEntity) world.getBlockEntity(pos);
-        grave.getCustomName(new ReadView());
+        grave.readComponents(ComponentMap.EMPTY,
+                ComponentChanges.builder().add(DataComponentTypes.CUSTOM_NAME, Text.literal("Here Lies ").append(getDisplayName())).build());
         grave.setStack(13, is);
 
         var player = (PlayerEntity) (Object) this;
