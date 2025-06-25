bb2cb4fdbce043ac1edb4cccc35b166a85e9309c
redboy1402 <101729153+redboy1402@users.noreply.github.com>
2025-06-25 13:23:53 +0800

    Moved zoom functionality to Custom Keybinds Class

--- src/client/java/com/example/mixin/client/GameRendererMixin.java
+++ src/client/java/com/example/mixin/client/GameRendererMixin.java
@@ -14,27 +14,13 @@ import com.example.CustomKeybinds;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-    final float clientFov = MinecraftClient.getInstance().options.getFov().getValue();
-    final float zoomedFov = 30f;
-    float zoomAmount = 0f;
+
     CustomKeybinds keybinds = new CustomKeybinds();
-
-    @Unique
-    float lerp(float a, float b, float t) {
-        return ( a * (1f - t)) + (b * t);
-    }
-
-    @Unique
-    float clamp01(float val) {
-        if (val < 0) {return 0;}
-        else if (val > 1) {return 1;}
-        else {return val;}
-    }
 
     @Inject(at=@At("HEAD"), method="getFov", cancellable = true)
     void FOVInject(CallbackInfoReturnable<Float> cir) {
-        zoomAmount += keybinds.isZoomed() ? -0.1f : 0.1f;
-        zoomAmount = clamp01(zoomAmount);
-        cir.setReturnValue(lerp(zoomedFov, clientFov, zoomAmount));
+        keybinds.setClientOptions(MinecraftClient.getInstance().options);
+        keybinds.zoom();
+        cir.setReturnValue(keybinds.getZoom());
     }
 }
